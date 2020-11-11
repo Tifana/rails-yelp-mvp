907f3343913a8a1ac88c4b908d75e5bdbda13efe
tifana rosa <tifanarosa@gmail.com>
2020-11-12 00:41:13 +0100

    working on seed (got 2 restaurants)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Creating 100 fake restaurants...'
+100.times do
+  restaurant = Restaurant.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    rating:  rand(0..5)
+  )
+  restaurant.save!
+end
+puts 'Finished!'
